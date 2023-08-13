d9371937c8010a3a6088fe9d177bced4562a2118
yuvalgos <yuvalgos@gmail.com>
2023-08-13 15:17:46 +0300

    added experimenting example, need to improve

---
+++ experimenting_example.py
@@ -0,0 +1,30 @@
+from math import pi
+import numpy as np
+from grasp_simulator.grasp_simulator import GraspSimulator
+import tqdm
+from tqdm.contrib import itertools
+
+
+# try to grasp at grid of positions x = [-0.05, 0.05], y = [-0.05, 0.05], z = [0.03, 0.1]
+# generate a list of all coordinates:
+x = np.linspace(-0.05, 0.05, 3)
+y = np.linspace(-0.05, 0.05, 3)
+z = np.array([0.03, 0.1])
+grasping_positions = np.array(np.meshgrid(x, y, z)).T.reshape(-1, 3)
+
+# for each position try to grasp from different directions:
+rx = np.linspace(-pi, pi, 5)
+ry = np.linspace(-pi, pi, 5)
+rz = np.linspace(-pi, pi, 5)
+grasping_orientations = np.array(np.meshgrid(rx, ry, rz)).T.reshape(-1, 3)
+
+print(f"trying to grasp at {len(grasping_positions)} positions and {len(grasping_orientations)} orientations, total"
+      f"of {len(grasping_positions) * len(grasping_orientations)} grasps")
+
+
+simulator = GraspSimulator(launch_viewer=False, real_time=False)
+
+# will take less than an hour on crappy laptop:
+for grasp_position, grasp_orientation in itertools.product(grasping_positions, grasping_orientations):
+        result = simulator.try_grasp(grasp_position, grasp_orientation)
+
--- scratch.py
+++ scratch.py
@@ -5,43 +5,9 @@ import matplotlib.pyplot as plt
 from grasp_simulator.utils import plot_depth_image, plot_lidar_im_depth_tri
 
 
-simulator = GraspSimulator(launch_viewer=True, real_time=True)
+simulator = GraspSimulator(launch_viewer=False, real_time=False)
 simulator.reset()
 simulator.simulate_seconds(0.2)  # best to give it a bit of time to settle
 
-# lidar is used through simulator. sensors
-sensors = simulator.sensors
-
-# this method gets a lidar output that is form the side of the object, at a given height.
-# The lidar is "placed" 25 cm away from the table origin.
-side_lidar_out = sensors.get_side_lidar_output(height=0.05)
-plot_depth_image(np.expand_dims(side_lidar_out, 0), name="side lidar")
-# output is array of size 45, for 45 degrees of the lidar.
-
-# for debugging, you can also get images from a camera that is placed at the same location as the lidar with
-# get_images=True. you will get lidar output, image, and depth image.
-lidar, im, depth_im = sensors.get_side_lidar_output(height=0.05, get_images=True)
-plot_lidar_im_depth_tri(np.expand_dims(side_lidar_out, 0), im, depth_im, name="side lidar")
-
-# there is also a predefined front lidar which is placed 25 cm front of the object.
-lidar, im, depth_im = sensors.get_front_lidar_output(height=0.05, get_images=True)
-plot_lidar_im_depth_tri(np.expand_dims(lidar, 0), im, depth_im, name="front lidar")
-
-# different height
-lidar, im, depth_im = sensors.get_side_lidar_output(height=0.15, get_images=True)
-plot_lidar_im_depth_tri(np.expand_dims(lidar, 0), im, depth_im, name="side lidar height=15cm")
-
-# custom camera settings:
-# you can get lidar (and images) from any pose of camera you want using get_lidar_output_by_parameters.
-lidar, im, depth_im = sensors.get_lidar_output_by_parameters(lookat=[0.05, 0, 0.1],
-                                                             distance=0.4,
-                                                             elevation=-45,
-                                                             azimuth=45,
-                                                             get_images=True)
-plot_lidar_im_depth_tri(np.expand_dims(lidar, 0), im, depth_im, name="custom camera", depth_threshold=1)
-
 
 simulator.simulate_seconds(30)  # leave window open for 30 more seconds
-
-# TODO:
-# - other objects
